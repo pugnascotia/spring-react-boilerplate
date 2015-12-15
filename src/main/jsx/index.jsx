46d118a2ddb060146803c5d48a68635a3aecaeac
Rory Hunter <rory.hunter@blackpepper.co.uk>
2015-12-15 12:40:49 +0000

    Implement React server-side rendering

--- src/main/jsx/index.jsx
+++ src/main/jsx/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { renderToString } from 'react-dom/server';
 
 import Hello from './components/Hello';
 
 if (typeof window !== 'undefined') {
     render(<Hello/>, document.getElementById('app'));
+}
+
+export function renderApp() {
+    return renderToString(<Hello/>);
 }
