281fe27e70cd185f2bf9d011b060e48e824e34e1
Rory Hunter <rory.hunter@blackpepper.co.uk>
2015-12-16 10:20:02 +0000

    Implement caching in the server render function.

--- src/main/resources/static/js/render.js
+++ src/main/resources/static/js/render.js
@@ -3,9 +3,21 @@ var render = (function () {
     // http://www.slideshare.net/SpringCentral/serverside-javascript-with-nashorn-and-spring
     var serializer = (new Packages.com.fasterxml.jackson.databind.ObjectMapper()).writer();
 
-    return function(template, model) {
+    /* Basic cache implementation. Shouldn't need to worry about eviction and such
+     * since we'll only use a small number of templates, given our React front-end. */
+    var cache = {};
+
+    function getEJS(template, url) {
+        if (!cache[url]) {
+            cache[url] = new EJS({text: template});
+        }
+
+        return cache[url];
+    }
+
+    return function(template, model, url) {
         var json =  serializer.writeValueAsString(model);
 
-        return new EJS({text: template}).render({data: JSON.parse(json), json: json});
+        return getEJS(template, url).render({data: JSON.parse(json), json: json});
     };
 })();
