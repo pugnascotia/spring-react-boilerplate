3a22a6665373807afc7709adc9eac33cf868e405
Rory Hunter <rory.hunter@blackpepper.co.uk>
2015-12-17 09:44:37 +0000

    Change indentation of AddComment

--- src/main/jsx/components/AddComment.jsx
+++ src/main/jsx/components/AddComment.jsx
@@ -6,36 +6,40 @@ import { addComment } from '../actions';
 
 class AddComment extends React.Component {
 
-    addComment(author, content) {
-        this.props.dispatch(addComment(author, content));
-    }
+  addComment(author, content) {
+    this.props.dispatch(addComment(author, content));
+  }
 
-    handleOnClick() {
-        const author = this.refs.author;
-        const content = this.refs.content;
+  handleOnClick() {
+    const author = this.refs.author;
+    const content = this.refs.content;
 
-        this.addComment(author.value.trim(), content.value.trim());
+    this.addComment(author.value.trim(), content.value.trim());
 
-        author.value = '';
-        content.value = '';
+    author.value = '';
+    content.value = '';
 
-        this.context.history.pushState(null, `/`);
-    }
+    this.context.history.pushState(null, `/`);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Add Comment</h1>
-                <div>
-                    <IndexLink to="/" className="btn btn-primary">Back</IndexLink>
-                </div>
-                <label>Author:</label>
-                <input type="text" size="50" ref="author"/>
-                <label>Comment:</label>
-                <input type="text" size="50" ref="content"/>
-                <button type="submit" onClick={e => this.handleOnClick(e)}>Submit</button>
-            </div>);
-    }
+  render() {
+    return (
+      <div>
+        <h1>Add Comment</h1>
+        <div>
+          <IndexLink to="/" className="btn btn-primary">Back</IndexLink>
+        </div>
+        <div>
+          <label>Author:</label>
+          <input type="text" size="50" ref="author"/>
+        </div>
+        <div>
+          <label>Comment:</label>
+          <input type="text" size="50" ref="content"/>
+        </div>
+        <button type="submit" onClick={e => this.handleOnClick(e)}>Submit</button>
+      </div>);
+  }
 }
 
 AddComment.contextTypes = { history: RouterPropTypes.history };
