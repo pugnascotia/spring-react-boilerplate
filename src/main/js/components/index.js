806284a92d113ce25f881cae2fe37579181a243c
Rory Hunter <rory.hunter@blackpepper.co.uk>
2016-01-04 12:39:16 +0000

    Add unknown route handling

--- src/main/js/components/index.js
+++ src/main/js/components/index.js
@@ -1,4 +1,5 @@
 export { default as AddComment } from './AddComment';
 export { default as Comment } from './Comment';
 export { default as CommentList } from './CommentList';
+export { default as NotFound } from './NotFound';
 export { default as SignIn } from './SignIn';
