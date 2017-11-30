56c11208ae0786c47d2d3c9c47ded963ddec143a
Rory Hunter <roryhunter2@gmail.com>
2017-11-30 10:14:19 +0000

    Simplify renderer build script

--- src/main/js/react-renderer/build.js
+++ src/main/js/react-renderer/build.js
@@ -1,46 +1,37 @@
 const fs = require('fs');
 const babel = require('babel-core');
+const rimraf = require('rimraf');
 
-if (!fs.existsSync('./build')) {
-  fs.mkdirSync('./build');
+if (fs.existsSync('./build')) {
+  rimraf.sync('./build');
 }
+
+fs.mkdirSync('./build');
 
 if (!fs.existsSync('../../app/build/asset-manifest.json')) {
-  return console.log('../../app/build/asset-manifest.json does not exist - has the build run?')
+  console.warn('../../app/build/asset-manifest.json does not exist - has the build run?')
+  process.exit(1);
 }
 
-fs.writeFile('./build/renderer.js','// polyfill.js\n\n', function(err) {
-  if (err) { return console.log(err); }
-  fs.readFile('./polyfill.js', 'utf-8', function(err, data) {
-    if (err) { return console.log(err); }
-    // Running this through Babel breaks one of the polyfills. Until I work
-    // that out, merge it in manually.
-    fs.appendFile('./build/renderer.js', data, function (err) {
-      if (err) { return console.log(err); }
-      fs.appendFile('./build/renderer.js', '\n// asset-manifest.json\nvar assetManifest = \n', function (err) {
-        if (err) { return console.log(err); }
-        fs.readFile('../../app/build/asset-manifest.json', 'utf-8', function(err, data) {
-          if (err) { return console.log(err); }
-          // It's unreasonably difficult to load this file from the classpath at
-          // runtime, so I'm just adding it in at build time.
-          fs.appendFile('./build/renderer.js', data, function (err) {
-            if (err) { return console.log(err); }
-            fs.appendFile('./build/renderer.js', ';\n\n\n// renderer.js\n\n', function (err) {
-              if (err) { return console.log(err); }
-              fs.readFile('./renderer.js', 'utf-8', function(err, data) {
-                if (err) { return console.log(err); }
-                const transformedRendererJs = babel.transform(data, {
-                  "presets": ["es2015"],
-                  "plugins": ["transform-object-rest-spread"]
-                });
-                fs.appendFile('./build/renderer.js',transformedRendererJs.code, function (err) {
-                  if (err) { return console.log(err); }
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+const output = fs.createWriteStream('./build/renderer.js');
+
+output.write('// polyfill.js\n\n');
+
+// Running this through Babel breaks one of the polyfills. Until I work
+// that out, merge it in manually.
+output.write(fs.readFileSync('./polyfill.js', 'utf8'));
+
+// It's unreasonably difficult to load this file from the classpath at
+// runtime, so I'm just adding it in at build time.
+output.write('\n// asset-manifest.json\nvar assetManifest = ');
+output.write(fs.readFileSync('../../app/build/asset-manifest.json', 'utf8'));
+output.write(';\n\n');
+
+output.write('\n// renderer.js\n\n');
+const transformedRendererJs = babel.transformFileSync('./renderer.js', {
+  "presets": ["es2015"],
+  "plugins": ["transform-object-rest-spread"]
 });
+output.write(transformedRendererJs.code);
+
+output.end();
